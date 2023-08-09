965b5b7ed91d067c4042ce04ab8b5db6b77e892c
ADMIN <ADMIN@KAPIL>
2023-08-09 17:09:03 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace GroceryShopInventory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.RiceBox1 = new System.Windows.Forms.CheckBox();
+            this.SaltBox2 = new System.Windows.Forms.CheckBox();
+            this.SugarBox3 = new System.Windows.Forms.CheckBox();
+            this.WheetBox4 = new System.Windows.Forms.CheckBox();
+            this.OilBox5 = new System.Windows.Forms.CheckBox();
+            this.textrice = new System.Windows.Forms.TextBox();
+            this.textsalt = new System.Windows.Forms.TextBox();
+            this.textsugar = new System.Windows.Forms.TextBox();
+            this.textwheet = new System.Windows.Forms.TextBox();
+            this.textoil = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Item = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Qty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(50, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kapil Grocery Shop";
+            // 
+            // RiceBox1
+            // 
+            this.RiceBox1.AutoSize = true;
+            this.RiceBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RiceBox1.Location = new System.Drawing.Point(55, 75);
+            this.RiceBox1.Name = "RiceBox1";
+            this.RiceBox1.Size = new System.Drawing.Size(52, 17);
+            this.RiceBox1.TabIndex = 1;
+            this.RiceBox1.Text = "Rice";
+            this.RiceBox1.UseVisualStyleBackColor = true;
+            // 
+            // SaltBox2
+            // 
+            this.SaltBox2.AutoSize = true;
+            this.SaltBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaltBox2.Location = new System.Drawing.Point(55, 107);
+            this.SaltBox2.Name = "SaltBox2";
+            this.SaltBox2.Size = new System.Drawing.Size(48, 17);
+            this.SaltBox2.TabIndex = 2;
+            this.SaltBox2.Text = "Salt";
+            this.SaltBox2.UseVisualStyleBackColor = true;
+            // 
+            // SugarBox3
+            // 
+            this.SugarBox3.AutoSize = true;
+            this.SugarBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SugarBox3.Location = new System.Drawing.Point(55, 144);
+            this.SugarBox3.Name = "SugarBox3";
+            this.SugarBox3.Size = new System.Drawing.Size(59, 17);
+            this.SugarBox3.TabIndex = 3;
+            this.SugarBox3.Text = "Sugar";
+            this.SugarBox3.UseVisualStyleBackColor = true;
+            // 
+            // WheetBox4
+            // 
+            this.WheetBox4.AutoSize = true;
+            this.WheetBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WheetBox4.Location = new System.Drawing.Point(55, 183);
+            this.WheetBox4.Name = "WheetBox4";
+            this.WheetBox4.Size = new System.Drawing.Size(63, 17);
+            this.WheetBox4.TabIndex = 4;
+            this.WheetBox4.Text = "Wheet";
+            this.WheetBox4.UseVisualStyleBackColor = true;
+            // 
+            // OilBox5
+            // 
+            this.OilBox5.AutoSize = true;
+            this.OilBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OilBox5.Location = new System.Drawing.Point(55, 224);
+            this.OilBox5.Name = "OilBox5";
+            this.OilBox5.Size = new System.Drawing.Size(41, 17);
+            this.OilBox5.TabIndex = 5;
+            this.OilBox5.Text = "Oil";
+            this.OilBox5.UseVisualStyleBackColor = true;
+            // 
+            // textrice
+            // 
+            this.textrice.Location = new System.Drawing.Point(129, 73);
+            this.textrice.Name = "textrice";
+            this.textrice.Size = new System.Drawing.Size(100, 20);
+            this.textrice.TabIndex = 6;
+            // 
+            // textsalt
+            // 
+            this.textsalt.Location = new System.Drawing.Point(129, 104);
+            this.textsalt.Name = "textsalt";
+            this.textsalt.Size = new System.Drawing.Size(100, 20);
+            this.textsalt.TabIndex = 7;
+            // 
+            // textsugar
+            // 
+            this.textsugar.Location = new System.Drawing.Point(129, 142);
+            this.textsugar.Name = "textsugar";
+            this.textsugar.Size = new System.Drawing.Size(100, 20);
+            this.textsugar.TabIndex = 8;
+            // 
+            // textwheet
+            // 
+            this.textwheet.Location = new System.Drawing.Point(129, 180);
+            this.textwheet.Name = "textwheet";
+            this.textwheet.Size = new System.Drawing.Size(100, 20);
+            this.textwheet.TabIndex = 9;
+            // 
+            // textoil
+            // 
+            this.textoil.Location = new System.Drawing.Point(129, 222);
+            this.textoil.Name = "textoil";
+            this.textoil.Size = new System.Drawing.Size(100, 20);
+            this.textoil.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "KG",
+            "GR"});
+            this.comboBox1.Location = new System.Drawing.Point(252, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(59, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "KG",
+            "GR"});
+            this.comboBox2.Location = new System.Drawing.Point(252, 103);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(59, 21);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "KG",
+            "GR"});
+            this.comboBox3.Location = new System.Drawing.Point(252, 140);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(59, 21);
+            this.comboBox3.TabIndex = 13;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "KG",
+            "GR"});
+            this.comboBox4.Location = new System.Drawing.Point(252, 179);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(59, 21);
+            this.comboBox4.TabIndex = 14;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "LT",
+            "ML"});
+            this.comboBox5.Location = new System.Drawing.Point(252, 220);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(59, 21);
+            this.comboBox5.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Item,
+            this.Price,
+            this.Qty,
+            this.Total});
+            this.dataGridView1.Location = new System.Drawing.Point(326, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(442, 171);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Item
+            // 
+            this.Item.HeaderText = "ITEM";
+            this.Item.Name = "Item";
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "PRICE";
+            this.Price.Name = "Price";
+            // 
+            // Qty
+            // 
+            this.Qty.HeaderText = "QTY";
+            this.Qty.Name = "Qty";
+            // 
+            // Total
+            // 
+            this.Total.HeaderText = "TOTAL";
+            this.Total.Name = "Total";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(154, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(321, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 25);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "TOTAL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(437, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textoil);
+            this.Controls.Add(this.textwheet);
+            this.Controls.Add(this.textsugar);
+            this.Controls.Add(this.textsalt);
+            this.Controls.Add(this.textrice);
+            this.Controls.Add(this.OilBox5);
+            this.Controls.Add(this.WheetBox4);
+            this.Controls.Add(this.SugarBox3);
+            this.Controls.Add(this.SaltBox2);
+            this.Controls.Add(this.RiceBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox RiceBox1;
+        private System.Windows.Forms.CheckBox SaltBox2;
+        private System.Windows.Forms.CheckBox SugarBox3;
+        private System.Windows.Forms.CheckBox WheetBox4;
+        private System.Windows.Forms.CheckBox OilBox5;
+        private System.Windows.Forms.TextBox textrice;
+        private System.Windows.Forms.TextBox textsalt;
+        private System.Windows.Forms.TextBox textsugar;
+        private System.Windows.Forms.TextBox textwheet;
+        private System.Windows.Forms.TextBox textoil;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Item;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Qty;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GroceryShopInventory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double sum = 0.0;
+            double price;
+            double qty;
+            double cal = 0.0;
+
+            if (RiceBox1.Checked)
+            {
+                string rice = RiceBox1.Text;
+                string msg = comboBox1.SelectedItem.ToString();
+                price = 140;
+                qty = int.Parse(textrice.Text);
+
+                if (msg.Equals("KG"))
+                {
+                    cal = (qty * price);
+                    this.dataGridView1.Rows.Add(rice,price,qty,cal);
+                }else
+                {
+                    cal = (qty / 1000 * price);
+                    this.dataGridView1.Rows.Add(rice, price, qty, cal);
+
+                }
+            }
+            if (SaltBox2.Checked)
+            {
+                string salt = SaltBox2.Text;
+                string msg = comboBox2.SelectedItem.ToString();
+                price = 50;
+                qty = int.Parse(textsalt.Text);
+
+                if (msg.Equals("KG"))
+                {
+                    cal = (qty * price);
+                    this.dataGridView1.Rows.Add(salt, price, qty, cal);
+                }
+                else
+                {
+                    cal = (qty / 1000 * price);
+                    this.dataGridView1.Rows.Add(salt, price, qty, cal);
+
+                }
+            }
+            if (SugarBox3.Checked)
+            {
+                string sugar = SugarBox3.Text;
+                string msg = comboBox3.SelectedItem.ToString();
+                price = 140;
+                qty = int.Parse(textsugar.Text);
+
+                if (msg.Equals("KG"))
+                {
+                    cal = (qty * price);
+                    this.dataGridView1.Rows.Add(sugar, price, qty, cal);
+                }
+                else
+                {
+                    cal = (qty / 1000 * price);
+                    this.dataGridView1.Rows.Add(sugar, price, qty, cal);
+
+                }
+            }
+            if (WheetBox4.Checked)
+            {
+                string wheet = WheetBox4.Text;
+                string msg = comboBox4.SelectedItem.ToString();
+                price = 140;
+                qty = int.Parse(textwheet.Text);
+
+                if (msg.Equals("KG"))
+                {
+                    cal = (qty * price);
+                    this.dataGridView1.Rows.Add(wheet, price, qty, cal);
+                }
+                else
+                {
+                    cal = (qty / 1000 * price);
+                    this.dataGridView1.Rows.Add(wheet, price, qty, cal);
+
+                }
+            }
+            if (OilBox5.Checked)
+            {
+                string oil = OilBox5.Text;
+                string msg = comboBox5.SelectedItem.ToString();
+                price = 200;
+                qty = int.Parse(textoil.Text);
+
+                if (msg.Equals("LT"))
+                {
+                    cal = (qty * price);
+                    this.dataGridView1.Rows.Add(oil, price, qty, cal);
+                }
+                else
+                {
+                    cal = (qty / 1000 * price);
+                    this.dataGridView1.Rows.Add(oil, price, qty, cal);
+
+                }
+            }
+
+            for (int row=0; row<dataGridView1.Rows.Count; row++)
+            {
+                sum = sum + Convert.ToInt32(dataGridView1.Rows[row].Cells[3].Value);
+
+            }
+
+            label3.Text = sum.ToString();
+        }
+    }
+}
